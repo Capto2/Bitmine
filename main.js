b921b7d6bb9142400ba126ede1d5628309f7f1fa
Augustine Jemifor <113078564+Capto2@users.noreply.github.com>
2022-11-24 03:48:23 +0100

    Day Two of the project
    Four nav Pages and few correction done

--- main.js
+++ main.js
@@ -7,14 +7,30 @@ const port = 3000;
 const app = express();
 
 //use modules
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.get("/", function(request, response){
+app.get("/", function (request, response) {
     response.render("index");
 });
+app.get("/about", function (request, response) {
+    response.render("about");
+});
 
-app.listen(port, function(){
+app.route("/login").get(function (request, response) {
+    response.render("login");
+}).post(function (request, response) { });
+
+app.route("/forgotten-password").get(function(request, response){
+    response.render("forgotten-password");
+}).post(function(request, response){});
+
+app.route("/register").get(function (request, response) {
+    response.render("register");
+}).post(function (request, response) { });
+
+
+app.listen(port, function () {
     console.log(`Server has started on port ${port}`);
 });
